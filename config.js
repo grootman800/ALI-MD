472b5ec1aad7ff4c4bece8eff22983913885a3d8
grootman800 <ggrootman68@gmail.com>
2025-06-21 00:50:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~vNkWlaBS#z-LhIosyQwlOOXwzqS0gOHN0JGG7Q3GAWewguNtQsZk",  
 //chatbot integration 
 CHATBOT: getConfig("CHATBOT") || "on",
 // add your Session Id 
